58eeb983a3bb0bea77a5ca5cd635fc25395fa0df
euanleith <leithe@tcd.ie>
2024-02-02 22:27:00 +0000

    Add - spawn point
    - Added object which sets player's spawn point to the current room on
      collision
    - Might want this to be trigger on player interact

--- RoomElement.cs
+++ RoomElement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public interface RoomElement
 {
+    // todo maybe make this abstract, since not all elements will require init/reset/disable
+    //  also might want to put bool enabled here
     public void Init();
     public void Reset();
     public void Disable();
---
+++ SpawnPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour, RoomElement
+{
+    // todo maybe want to activate on player interact (e.g. hold up while colliding with this)
+
+    public Room room;
+
+    public void Init() {}
+    public void Reset() {}
+    public void Disable() {}
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.layer == 12) { // Player layer
+            collider.gameObject.GetComponent<Health>().spawnRoom = room;
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+}
