0168f21ea54bbd42ecf6ff3cbf0396b775a4828d
euanleith <leithe@tcd.ie>
2024-02-02 20:30:23 +0000

    Refactor - created interface RoomElement
    - Added interface RoomElement which is implemented by Enemy and
      platforms.
    - Currently enemies and platforms are initialised, disabled, etc.
      separately. But this is the same task for both (and any other future
      room elements), so an interface can be created to avoid duplicate
      code.
    - Also removed Platform class, since it wasn't providing anything beyond
      RoomElement

---
+++ RoomElement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface RoomElement
+{
+    public void Init();
+    public void Reset();
+    public void Disable();
+}
