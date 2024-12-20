b39fb84e15a9953088d00f60cd391cfd31a74fa4
euanleith <leithe@tcd.ie>
2024-12-20 11:54:10 +0000

    add - minVolume to audiolooper
    - currently audio looper always fades in and out between the two looped
      tracks
    - but this isn't always desirable, e.g. for looping music, so setting
      minVolume to volume will remove this

--- Audio/AudioLooper.cs
+++ Audio/AudioLooper.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class AudioLooper : MonoBehaviour {
     public float overlapTime = 4f;
     public float volume = 0.5f;
+    public float minVolume = 0f; // todo replace with 'smoothing'?
     private AudioSource[] audioSources;
     private float elapsedTime = 0;
 
@@ -16,30 +18,19 @@ public class AudioLooper : MonoBehaviour {
 
     private void Update() 
     {
-        Debug.Log("0: " + audioSources[0].time + " " + (audioSources[0].clip.length - overlapTime) + " " + audioSources[0].volume);
-        Debug.Log("1: " + audioSources[1].time + " " + (audioSources[1].clip.length - overlapTime) + " " + audioSources[0].volume);
+        Overlap(audioSources[0], audioSources[1]);
+        Overlap(audioSources[1], audioSources[0]);
+    }
 
-        if (audioSources[0].time >= audioSources[0].clip.length - overlapTime) {
-            if (!audioSources[1].isPlaying) {
-                Debug.Log("playing 2");
-                audioSources[1].Play();
+    private void Overlap(AudioSource audioSource1, AudioSource audioSource2) {
+        if (audioSource1.time >= audioSource1.clip.length - overlapTime) {
+            if (!audioSource2.isPlaying) {
+                audioSource2.Play();
                 elapsedTime = 0;
             }
             elapsedTime += Time.deltaTime;
-                //audioSources[0].volume = volume * (audioSources[0].clip.length - audioSources[0].time);
-                audioSources[0].volume = Mathf.Lerp(volume, 0, elapsedTime / overlapTime);
-            audioSources[1].volume = Mathf.Lerp(0, volume, elapsedTime / overlapTime);
-        }
-        if (audioSources[1].time >= audioSources[1].clip.length - overlapTime) {
-            //Debug.Log(audioSources[1].time);
-            if (!audioSources[0].isPlaying) {
-                Debug.Log("playing 1");
-                audioSources[0].Play();
-                elapsedTime = 0;
-            }
-            elapsedTime += Time.deltaTime;
-            audioSources[0].volume = Mathf.Lerp(0, volume, elapsedTime / overlapTime);
-            audioSources[1].volume = Mathf.Lerp(volume, 0, elapsedTime / overlapTime);
+            audioSource1.volume = Mathf.Lerp(volume, minVolume, elapsedTime / overlapTime);
+            audioSource2.volume = Mathf.Lerp(minVolume, volume, elapsedTime / overlapTime);
         }
     }
 }
