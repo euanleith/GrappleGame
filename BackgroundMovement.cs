a108774a801e028963f52eb50a6d998602f60827
euanleith <leithe@tcd.ie>
2025-01-02 19:18:34 +0000

    fix - parallax
    - currently parallax doesn't work due to changes in the camera
    - currently camera changes position within the first few frames, which
      makes it have a high velocity, so positioning the parallaxed elements
      incorrectly
    - this should be fixed by not moving the camera at the start, but i've
      made a quick fix where i only set the position of the parallaxed
      elements after this initial camera velocity

--- BackgroundMovement.cs
+++ BackgroundMovement.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundMovement : MonoBehaviour
+public class BackgroundMovement : MonoBehaviour 
 {
-    public float movementWeight;
+    public new Camera camera;
+    public float parallaxEffect = 1f;
 
-    void Start()
-    {
-        
+    private Vector3 prevCameraPos;
+    private bool start = false;
+
+    void Start() {
+        prevCameraPos = camera.transform.position;
     }
 
-    void Update()
-    {
-        transform.Translate(Camera.main.velocity * Time.deltaTime / (GetComponent<SpriteRenderer>().sortingOrder * movementWeight), Camera.main.transform); 
+    void Update() {
+        Vector3 cameraVel = camera.transform.position - prevCameraPos;
+        if (cameraVel == Vector3.zero) start = true; // todo camera doesn't start in the room, so it changes position, and so has velocity, which messes up the initial transform.position here
+        if (start) {
+            transform.Translate(cameraVel * parallaxEffect, camera.transform);
+        }
+        prevCameraPos = camera.transform.position;
     }
 }
