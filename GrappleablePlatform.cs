32552782bde341c7599ca26ecfb326106825379c
euanleith <leithe@tcd.ie>
2025-08-04 00:04:08 +0100

    add grappleable platform

---
+++ GrappleablePlatform.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleablePlatform : MonoBehaviour
+{
+    public float speed = 2f;
+    public bool reusable = false;
+
+    Rigidbody2D rb;
+    bool used = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // todo integration with ropes
+    // todo these should reset with room
+    // todo should i have a variation which only ever moves in one direction, regardless of grapple direction?
+    public void OnCollisionEnterWithGrapple(Vector2 direction) {
+        Debug.Log(direction); // todo why does this give 0.71 for diagonals instead of 0.5?
+        if (!used || reusable) {
+            rb.velocity = -direction * speed;
+            used = true;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer != 12) { // Player layer
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
