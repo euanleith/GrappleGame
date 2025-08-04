9482c419d69c08903b64136869d805f37df36f3d
euanleith <leithe@tcd.ie>
2025-08-04 22:17:23 +0100

    add GG-14

--- BoundsEnter.cs
+++ BoundsEnter.cs
@@ -1,12 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoundsEnter : MonoBehaviour 
 {
-    // todo when entering a room from below, could start with some upwards speed
-    //  respawns when entering from below and there's no platform to land on are weird; maybe should respawn at end of previous room?
-
     CameraControls camControls;
     public Room room;
 
@@ -15,18 +10,48 @@ public class BoundsEnter : MonoBehaviour
         camControls = Camera.main.GetComponent<CameraControls>();
     }
     
-    void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.name == "Player") {
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.layer == 12) { // player
             camControls.room.Disable();
+            Vector2 playerNewPosition = collider.transform.position;
             room.Enable();
 
-            // todo clean up
-            camControls.room = room;
             Vector2 spawn = transform.Find("Spawn").position;
+            camControls.room = room;
             camControls.room.spawn = spawn;
-            collision.transform.position = spawn;
-            collision.gameObject.GetComponent<Health>().room = room;
-            collision.gameObject.GetComponent<Health>().room.spawn = spawn;
+            collider.GetComponent<Health>().room = room;
+            collider.GetComponent<Health>().room.spawn = spawn;
+
+            // todo nope, GetDirection is wrong
+            Vector2 direction = GetCardinalDirection(collider.transform, transform);
+            Debug.Log(direction);
+            Debug.Log("player current position: " + playerNewPosition);
+            Debug.Log("new spawn position: " + transform.Find("Spawn").position);
+            if (direction.x != 0) playerNewPosition.x = spawn.x; 
+            else if (direction.y != 0) playerNewPosition.y = spawn.y;
+            Debug.Log("new player position: " + playerNewPosition);
+            collider.transform.position = playerNewPosition;
         }
+    }
+
+    public Vector2 GetCardinalDirection(Transform a, Transform b) {
+        Vector2 direction = a.position - b.position;
+        Vector2 bLocal = b.InverseTransformDirection(direction);
+        Vector2 bScale = b.lossyScale;
+        Vector2 directionLocal = Divide(bLocal, bScale);
+
+        return Mathf.Abs(directionLocal.x) > Mathf.Abs(directionLocal.y) ? 
+            (directionLocal.x > 0 ? 
+                Vector2.right : Vector2.left) :
+            (directionLocal.y > 0 ? 
+                Vector2.up : Vector2.down);
+    }
+
+    public Vector3 Divide(Vector3 a, Vector3 b) {
+        return new Vector3(
+            a.x / b.x,
+            a.y / b.y,
+            a.z / b.z
+        );
     }
 }
